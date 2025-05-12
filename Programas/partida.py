91276d35e6c77d8ea4164596da53e2c2d96b2b48
relaxingEnd <miguelcolorado2007@gmail.com>
2025-05-12 18:09:16 -0500

    Algo

--- Programas/partida.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from turno import Turno
-
-class Partida:
-    def __init__(self, turnos: List[Turno]):
-        self.turnos = turnos
-
-    def __repr__(self):
-        return f"Partida(turnos={self.turnos})"
