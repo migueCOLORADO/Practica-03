77d8cd14d40933e85310c63e9ce4ff136fa282f5
relaxingEnd <miguelcolorado2007@gmail.com>
2025-05-12 16:50:00 -0500

    Mod | Archivo Main

--- Programas/Main.py
+++ Programas/Main.py
@@ -1,7 +1,7 @@
-from Parser import Parser
+from parse import Parser        # Importamos librerias
 
-if __name__ == '__main__':
-    texto = '1. e4 e5 2. Nf3 Nc6 3. Bb5 a6'
+if __name__ == '__main__':      # Ejecutamos todos los metodos 
+    texto = "1. e4 e5 2. Nf3 Nc6 3. Bb5 a6"     # 
     parser = Parser(texto)
-    partida = parser.parse()
+    partida = parser.parse(texto)
     print(partida)
